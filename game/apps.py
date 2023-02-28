3161cf8c02685b9b699832c7d28d0065a9d663ae
EstherRC <a01656060@tec.mx>
2023-02-28 12:05:04 -0600

    Up

--- game/apps.py
+++ game/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class calculadora(AppConfig):
+class game(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "videojuego"
+    name = "game"
